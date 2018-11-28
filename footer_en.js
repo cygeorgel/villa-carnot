90d896d7f93f55d6cec6422125c0fb35a94a7556
Cyrille GEORGEL <cyrille.georgel@gmail.com>
2018-11-28 23:18:24 +0000

    en website version

---
+++ footer_en.js
@@ -0,0 +1,5 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script>
+    $(document).ready(function(){$("div.vc_custom_1442748177245").load('http://en.villacarnot.com/form.php');
+    });
+</script>
